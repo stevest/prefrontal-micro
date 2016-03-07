a035c013907c29ba53d16800549d28222f208611
stevest <stamatiad.st@gmail.com>
2016-03-08 00:50:56 +0200

    Add git sha1 to each run.

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -15,12 +15,15 @@ dirtygit=$(( $dirtygit + $(git status --porcelain 2>/dev/null| grep "^A" | wc -l
 dirtygit=$(( $dirtygit + $(git status --porcelain 2>/dev/null| grep "^\?" | wc -l) ))
 
 
+	gitsha1=`git rev-parse HEAD`
 if [[ $dirtygit > 0 ]]; then
 	echo "Can not continue with run when git repo is dirty. Exiting..."
+	echo "Current HEAD is: ${gitsha1}"
 	#exit 1
 else
 	echo "Git repo is clean. Continue run with SHA1: "
-	echo `git rev-parse HEAD`
+	gitsha1=`git rev-parse HEAD`
+	echo $gitsha1
 fi
 
 source /opt/gridengine/default/common/settings.csh
@@ -42,19 +45,19 @@ echo `pwd`
 
 parallel="1"
 #All nodes are: 288
-nodes="288"
+nodes="64"
 ##jobname="STR_N100_S6_STC0"
 jobstdout=""
 cluster="0"
 # 0=Random, 1=Structured
-exp="0"
+exp="1"
 clustbias="0.0"
 startRun="0"
-endRun="99"
+endRun="0"
 #naming convention in ten characters:
 if [ "$exp" == "1" ]; then
-	#jobname="Ss20c${cluster}r"
-	jobname="GABAb015NEWBGST_Ss20c${cluster}r"
+	jobname="TEST"
+	#jobname="GABAb015NEWBGST_Ss20c${cluster}r"
 else
 	jobname="GABAb015NEWBGST_Rs20c${cluster}r"
 fi
@@ -111,7 +114,7 @@ do
 		mkdir -p $outputDir;
 	fi
 	## Submit as Job in Sun Grid Engine:
-	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte 24-$nodes -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
+	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte $nodes -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1'\\\"\)" -c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 	## Run locally:
 	##/opt/openmpi/bin/mpirun -v -n $nodes /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c 'execute1("strdef JOBNAME, JOBDIR")' -c 'execute1("JOBNAME = \"'$jobname'\"")' -c 'execute1("JOBDIR = \"'$outputDir'\"")' -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 	#qsub -b y -S /bin/bash -V -N postjob -pe orte 1 -hold_jid $uniquejobname postjob.sh $outputFile $uniquejobname
