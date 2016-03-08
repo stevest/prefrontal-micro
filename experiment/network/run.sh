6a2b8fc295667d5ae6911733837a1f328dd76797
stevest <stamatiad.st@gmail.com>
2016-03-08 18:54:22 +0200

    New code. one sha1 per simulation.

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -27,15 +27,6 @@ else
 fi
 
 source /opt/gridengine/default/common/settings.csh
-##$ -S /bin/sh
-##$ -V
-###$ -cwd ##Does NOT HAVE the /home/ prefix; causing error
-##$ -N JobStefanos
-##$ -o /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/flow.out -j y
-##$ -l h=!compute-0-1&!compute-0-3&!compute-0-4&!compute-0-6&!compute-0-7&!compute-0-12&!compute-0-13&!compute-0-14&!compute-0-15
-##$ -pe orte 90
-##$ -R y
-##sge_o_workdir="/home/cluster/stefanos/Documents/Github/prefrontal-micro/experiment/network/"
 
 simhome="/home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network"
 storagehome="/home/cluster/stefanos/Documents/Glia"
@@ -51,6 +42,7 @@ jobstdout=""
 cluster="0"
 # 0=Random, 1=Structured
 exp="1"
+sn="1"
 clustbias="0.0"
 startRun="0"
 endRun="0"
@@ -114,7 +106,7 @@ do
 		mkdir -p $outputDir;
 	fi
 	## Submit as Job in Sun Grid Engine:
-	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte $nodes -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1'\\\"\)" -c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
+	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte $nodes -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1, SN'\\\"\)" -c "execute1\(\\\"'SN = \\\"$sn\\\"'\\\"\)" -c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 	## Run locally:
 	##/opt/openmpi/bin/mpirun -v -n $nodes /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c 'execute1("strdef JOBNAME, JOBDIR")' -c 'execute1("JOBNAME = \"'$jobname'\"")' -c 'execute1("JOBDIR = \"'$outputDir'\"")' -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 	#qsub -b y -S /bin/bash -V -N postjob -pe orte 1 -hold_jid $uniquejobname postjob.sh $outputFile $uniquejobname
