b54c7d0aa6c3d7288ee56ff02d3a56261f1b9fe8
stevest <stamatiad.st@gmail.com>
2016-06-01 08:42:46 +0300

    Make PID global

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -39,14 +39,15 @@ parallel="1"
 nodes="288"
 ##jobname="STR_N100_S6_STC0"
 jobstdout=""
-cluster="3"
+cluster="1"
 # 0=Random, 1=Structured
-exp="1"
+exp="0"
 sn="2"
 clustbias="0.0"
 startRun="0"
 endRun="49"
-custom_jobs=(3 5 6 7 8 9 10 11 12 13 14 15 17 18 19 20 27 28 29 31 36 38 43 49)
+VARPID="0.5"
+custom_jobs=(17)
 #naming convention in ten characters:
 if [ "$exp" == "1" ]; then
 	#jobname="test_nosge"
@@ -92,13 +93,13 @@ jobstdout="$jobstdout\\\n=======================================================
 #Working with STDOUT:#qsub -b y -S /bin/bash -V -N $jobname -o /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/$outputFile.out -j y -pe orte $nodes -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef STDOUT, JOBNAME'\\\"\)" -c "execute1\(\\\"'STDOUT = \\\"$jobstdout\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$jobname\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "SIMPLIFIED=$simplified" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "ST=$state" -c "ID=$id" -c "SN=$sn" -c "VCLAMP=$vclamp" -c "ISBINARY=$binary" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 echo $jobstdout
 #POSIXLY_CORRECT=0
-for run in $(seq $startRun $endRun);
-#for run in "${custom_jobs[@]}"
+#for run in $(seq $startRun $endRun);
+for run in "${custom_jobs[@]}"
 do
 	echo $run;
 	uniquejobname="${jobname}${run}"
 	outputFile=$uniquejobname.out
-	outputDir="${storagehome}/${uniquejobname}_PID75"
+	outputDir="${storagehome}/${uniquejobname}_PID50"
 	echo "Output Job directory is:"
 	echo $outputDir
 	if [ -d $outputDir ]; then
@@ -108,7 +109,7 @@ do
 		mkdir -p $outputDir;
 	fi
 	## Submit as Job in Sun Grid Engine:
-	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte 2-$nodes -p -3 -M mpi@dendrites.gr -m bea -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1, SN'\\\"\)" -c "execute1\(\\\"'SN = \\\"$sn\\\"'\\\"\)" -c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
+	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte 2-$nodes -p -3 -M mpi@dendrites.gr -m bea -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1, SN'\\\"\)" -c "execute1\(\\\"'SN = \\\"$sn\\\"'\\\"\)" -c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" -c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" -c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" -c "VARPID=$VARPID" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
 	## Run without scheduler:
 	#/opt/openmpi/bin/mpirun -v -n $nodes --host compute-0-1,compute-0-3 /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c 'execute1("strdef JOBNAME, JOBDIR, GITSHA1, SN")' -c "execute1(\"SN = \\\"$sn\\\"\")" -c "execute1(\"GITSHA1 = \\\"$gitsha1\\\"\")" -c "execute1(\"JOBNAME = \\\"$uniquejobname\\\"\")" -c "execute1(\"JOBDIR = \\\"$outputDir\\\"\")" -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc | tee "${outputDir}/${outputFile}" 
 	
