a16fd132c8a6b0f10c345e05212ed6ee526a701a
stefanos <stefanos@bioinf-dhcp4.imbb.forth.gr>
2017-01-13 13:32:03 +0200

    Make nhPoisson stimulation more explicit and correct bugs.

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash ##Make sure that before each run git repo is clean, so ##one can track each run to its source code:
+#!/usr/bin/sh ##Make sure that before each run git repo is clean, so ##one can track each run to its source code:
 ##Documentation in: https://git-scm.com/docs/git-status
 dirtygit="0"
 ##Files MODIFIED since index:
@@ -27,12 +27,17 @@ fi
 ##source /opt/gridengine/default/common/settings.csh
 simhome=`pwd`
 simhome="${simhome}/"
-storagehome="/home/stefanos/Documents/Glia"
+simglia="/home/stefanos/Documents/Glia"
+## Define neuron executable. This is the git repo inside ~/Libraries.
+nrn_executable="nrn_fork"
 echo "Currently at directory:"
 echo $simhome
 echo `pwd`
+echo "Using NEURON executable ${nrn_executable}."
 
 parallel="1"
+## Use scheduler or directly run with mpi:
+schedule="0"
 #All nodes are:312 
 nodes="1" ##jobname="STR_N100_S6_STC0" jobstdout=""
 cluster="6"
@@ -44,8 +49,6 @@ sn="10"
 clustbias="1"
 excitbias="10"
 inhibias="1"
-BGe = 1
-BGi = 1
 Cl="7"
 Fs="1"
 startRun="0"
@@ -79,8 +82,9 @@ for BGe in $(seq 2 2); do
 	fi
 	uniquejobname="${jobname}${run}"
 	outputFile=$uniquejobname.out
-	#outputDir="${storagehome}/${uniquejobname}_PID25"
-	outputDir="${storagehome}/${uniquejobname}"
+	#outputDir="${simglia}/${uniquejobname}_PID25"
+	outputDir="${simglia}/${uniquejobname}"
+	simglia="${simglia}/"
 	echo "Output Job directory is:"
 	echo $outputDir
 	if [ -d $outputDir ]; then
@@ -90,36 +94,69 @@ for BGe in $(seq 2 2); do
 		mkdir -p $outputDir;
 	fi
 	## Submit as Job in Sun Grid Engine:
+	if [ "$schedule" == "1" ]; then
 	qsub -b y -S /bin/bash -V -N $uniquejobname -o "${outputDir}/${outputFile}" -j y -pe orte 24-$nodes -p -3 -M mpi@dendrites.gr -m bea -R y /opt/openmpi/bin/mpirun /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/myspecial -nobanner -mpi \
--c "RUN=$run" \
--c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1, SN'\\\"\)" \
--c "execute1\(\\\"'SN = \\\"$sn\\\"'\\\"\)" \
--c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" \
--c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" \
--c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" \
--c "PARALLEL=$parallel" \
--c "CLUSTER_ID=$cluster" \
--c "EXPERIMENT=$exp" \
--c "CLUSTBIAS=$clustbias" \
--c "EXCITBIAS=$excitbias" \
--c "INHIBIAS=$inhibias" \
--c "FS=$Fs" \
--c "CL=$Cl" \
--c "BGE=$BGe" \
--c "BGI=$BGi" \
--c "VARPID=$VARPID" \
-/home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
+	-c "RUN=$run" \
+	-c "execute1\(\\\"'strdef JOBNAME, JOBDIR, GITSHA1, SN'\\\"\)" \
+	-c "execute1\(\\\"'SN = \\\"$sn\\\"'\\\"\)" \
+	-c "execute1\(\\\"'GITSHA1 = \\\"$gitsha1\\\"'\\\"\)" \
+	-c "execute1\(\\\"'JOBNAME = \\\"$uniquejobname\\\"'\\\"\)" \
+	-c "execute1\(\\\"'JOBDIR = \\\"$outputDir\\\"'\\\"\)" \
+	-c "PARALLEL=$parallel" \
+	-c "CLUSTER_ID=$cluster" \
+	-c "EXPERIMENT=$exp" \
+	-c "CLUSTBIAS=$clustbias" \
+	-c "EXCITBIAS=$excitbias" \
+	-c "INHIBIAS=$inhibias" \
+	-c "FS=$Fs" \
+	-c "CL=$Cl" \
+	-c "BGE=$BGe" \
+	-c "BGI=$BGi" \
+	-c "VARPID=$VARPID" \
+	/home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
+
+	else
+## Run local mpi without scheduler:
+PATH=/home/stefanos/Libraries/${nrn_executable}/x86_64/bin:$PATH
+LD_LIBRARY_PATH=/home/stefanos/Libraries/${nrn_executable}/x86_64/lib:$LD_LIBRARY_PATH
+export PATH
+export LD_LIBRARY_PATH
+echo "NEURON executable in PATH before mpirun is: "
+echo `which nrniv`
+
+/opt/openmpi/bin/mpirun -np 1 \
+	-x PATH \
+	-x LD_LIBRARY_PATH \
+	/home/stefanos/Documents/SourceTree/prefrontal-micro/$mechanisms/myspecial ${nrn_executable} -nobanner -mpi \
+	-c "RUN=$run" \
+	-c 'execute1("strdef JOBNAME, JOBDIR, GITSHA1, SN, SIMHOME, SIMGLIA")' \
+	-c 'execute1("SN = \"'$sn'\"")' \
+	-c 'execute1("GITSHA1 = \"'$gitsha1'\"")' \
+	-c 'execute1("JOBNAME = \"'$uniquejobname'\"")' \
+	-c 'execute1("JOBDIR = \"'$outputDir'\"")' \
+	-c 'execute1("SIMHOME = \"'$simhome'\"")' \
+	-c 'execute1("SIMGLIA = \"'$simglia'\"")' \
+	-c "PARALLEL=$parallel" \
+	-c "CLUSTER_ID=$cluster" \
+	-c "EXPERIMENT=$exp" \
+	-c "CLUSTBIAS=$clustbias" \
+	-c "EXCITBIAS=$excitbias" \
+	-c "INHIBIAS=$inhibias" \
+	-c "FS=$Fs" \
+	-c "CL=$Cl" \
+	-c "BGE=$BGe" \
+	-c "BGI=$BGi" \
+	-c "VARPID=$VARPID" \
+	/home/stefanos/Documents/SourceTree/prefrontal-micro/experiment/network/final.hoc
+	fi
 	
-	## Run locally:
-	##/opt/openmpi/bin/mpirun -v -n $nodes /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/$mechanisms/x86_64/special -nobanner -mpi -c "RUN=$run" -c 'execute1("strdef JOBNAME, JOBDIR")' -c 'execute1("JOBNAME = \"'$jobname'\"")' -c 'execute1("JOBDIR = \"'$outputDir'\"")' -c "PARALLEL=$parallel" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "CLUSTBIAS=$clustbias" /home/cluster/stefanos/Documents/GitHub/prefrontal-micro/experiment/network/final.hoc 
-	#qsub -b y -S /bin/bash -V -N postjob -pe orte 1 -hold_jid $uniquejobname postjob.sh $outputFile $uniquejobname
 done
 done
 done
  
 else
 
-nohup ../../$mechanisms/x86_64/special -nobanner -c "PARALLEL=$parallel" -c "SIMPLIFIED=$simplified" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "ST=$state" -c "ID=$id" -c "SN=$sn" -c "VCLAMP=$vclamp" -c "ISBINARY=$binary" -c "CLUSTBIAS=$clustbias" final.hoc | tee nohup.out &
+nohup ../../$mechanisms/myspecial -nobanner -c "PARALLEL=$parallel" -c "SIMPLIFIED=$simplified" -c "CLUSTER_ID=$cluster" -c "EXPERIMENT=$exp" -c "ST=$state" -c "ID=$id" -c "SN=$sn" -c "VCLAMP=$vclamp" -c "ISBINARY=$binary" -c "CLUSTBIAS=$clustbias" final.hoc | tee nohup.out &
 
 fi
 echo Run script reached end.
