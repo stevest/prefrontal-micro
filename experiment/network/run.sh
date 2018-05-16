d112c251eea8faf872be88d9d413a4c1c8f962c0
stevest <stamatiad.st@gmail.com>
2018-05-16 08:23:00 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -129,10 +129,10 @@ gababfactor="15"
 pv2pc="4"
 pc2pc="60"
 # Pass simulation stop externally in seconds:
-tstop_sec="3"
-for pc2pc in $(seq 105 5 200); do
+tstop_sec="7"
+for pc2pc in $(seq 120 120); do
 #for pv2pc in $(seq 52 52); do
-#for cluster in $(seq 0 0); do
+for cluster in $(seq 0 19); do
 ##for gababfactor in $(seq 26 34); do
 #for excitbias in $(seq 25 25); do
 for inhibias in $(seq 4 4); do
