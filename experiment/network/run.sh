3fe0e6eefa9d5a5e1dcd0247c88dbb1f3434b11f
stevest <stamatiad.st@gmail.com>
2018-05-26 12:14:01 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -124,18 +124,18 @@ clpid="0.45"
 stimfreq="60"
 stimnoise="0.0"
 inhibias="1"
-excitbias="600"
+excitbias="425"
 gababfactor="1"
 pv2pc="4"
 pc2pc="1"
 # Pass simulation stop externally in seconds:
-tstop_sec="2.5"
+tstop_sec="7"
 #for pc2pc in $(seq 62 2 120); do
 #for pv2pc in $(seq 52 52); do
-for cluster in $(seq 0 0); do
+for cluster in $(seq 0 19); do
 ##for gababfactor in $(seq 26 34); do
 #for excitbias in $(seq 25 25); do
-for inhibias  in $(seq 1 1 4); do
+for inhibias  in $(seq 1 1); do
 ##for erf in "${erf_array[@]}"; do
 #	cluster="${run}"
 	if [ "$exp" == "1" ]; then
