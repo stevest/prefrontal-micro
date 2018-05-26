8d4ef914d1c377e8ccf4c3fe47b47f9d1b76eb62
stevest <stamatiad.st@gmail.com>
2018-05-26 10:16:55 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -135,7 +135,7 @@ tstop_sec="2.5"
 for cluster in $(seq 0 0); do
 ##for gababfactor in $(seq 26 34); do
 #for excitbias in $(seq 25 25); do
-for gababfactor  in $(seq 1 1 14); do
+for inhibias  in $(seq 1 1 4); do
 ##for erf in "${erf_array[@]}"; do
 #	cluster="${run}"
 	if [ "$exp" == "1" ]; then
