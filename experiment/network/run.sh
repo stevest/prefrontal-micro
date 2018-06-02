de84db393a042c34075b2a5c1881715b6990716a
stevest <stamatiad.st@gmail.com>
2018-06-02 10:48:24 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -123,7 +123,7 @@ locpid="7"
 clpid="0.45"
 stimfreq="60"
 stimnoise="0.0"
-inhibias="3"
+inhibias="4"
 excitbias="1"
 gababfactor="15"
 pv2pc="4"
@@ -134,7 +134,7 @@ loccl="1"
 #for pc2pc in $(seq 62 2 120); do
 #for pv2pc in $(seq 52 52); do
 ##for gababfactor in $(seq 26 34); do
-for loccl in $(seq 1 1); do
+#for loccl in $(seq 10 10); do
 for cluster in $(seq 0 0); do
 #for inhibias  in $(seq 6 6); do
 ##for erf in "${erf_array[@]}"; do
@@ -252,7 +252,7 @@ echo `which nrniv`
 	fi
 	
 done
-done
+#done
 ##done
 ##done
  
