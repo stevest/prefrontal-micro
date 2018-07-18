a27ba83800b861ec85699535cb79b8b6c1707e56
stevest <stamatiad.st@gmail.com>
2018-07-18 11:02:15 +0300

    RUN AUTOCOMMIT

--- experiment/network/runnmdaspikes.sh
+++ experiment/network/runnmdaspikes.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh 
 for nmdatau in $(seq 10 10); do
-for isi in $(seq 5 5); do
-for number in $(seq 1 5); do
-../../mechanism_simple/x86_64/special -nobanner -c "ISI=$isi" -c "NUMBER=$number" -c "NMDATAU=$nmdatau" nmda_spikes.hoc 
+for isi in $(seq 1 1); do
+for number in $(seq 1 1); do
+for synapses in $(seq 5 5 30); do
+../../mechanism_simple/x86_64/special -nobanner -c "SYNAPSES=$synapses" -c "ISI=$isi" -c "NUMBER=$number" -c "NMDATAU=$nmdatau" nmda_spikes.hoc 
+done
 done
 done
 done
