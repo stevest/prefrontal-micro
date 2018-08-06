bb29b0f71379aa6fb66a3f9d1e4f66675e9502d3
stevest <stamatiad.st@gmail.com>
2018-08-06 11:23:31 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -57,7 +57,7 @@ cluster="0"
 # 0=Random, 1=Structured
 exp="1"
 ## Serial number of network (RNG) in MATLAB:
-sn="21"
+sn="20"
 ## Ean einai clustered oi synapseeis stous dendrites:
 clustbias="1"
 ## Excitation /inhibition bias (multiplier factor) gia PC2PC synapses
@@ -123,20 +123,20 @@ locpid="7"
 clpid="0.45"
 stimfreq="60"
 stimnoise="0.0"
-inhibias="4"
+inhibias="3"
 excitbias="1"
 gababfactor="15"
 pv2pc="4"
 pc2pc="4"
 # Pass simulation stop externally in seconds:
-tstop_sec="3"
+tstop_sec="5"
 loccl="1"
 #for pc2pc in $(seq 2 2 20); do
 #for pv2pc in $(seq 52 52); do
 ##for gababfactor in $(seq 26 34); do
-#for loccl in $(seq 1 1); do
-for cluster in $(seq 1 4); do
-for inhibias  in $(seq 3 4); do
+for loccl in $(seq 1 10); do
+for cluster in $(seq 0 9); do
+#for inhibias  in $(seq 3 4); do
 ##for erf in "${erf_array[@]}"; do
 #	cluster="${run}"
 	if [ "$exp" == "1" ]; then
