fc28dd6d37c1be3431bf849b37cc1b744aef6d0f
stevest <stamatiad.st@gmail.com>
2018-10-22 19:37:16 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -139,7 +139,7 @@ cluster="0"
 ##for gababfactor in $(seq 26 34); do
 for loccl in $(seq 1 10); do
 #for stimnoise in $(seq 0.1 0.1 0.5); do
-for cluster in $(seq 0 19); do
+for cluster in $(seq 20 49); do
 #for inhibias  in $(seq 3 0.5 9); do
 ##for erf in "${erf_array[@]}"; do
 #	cluster="${run}"
