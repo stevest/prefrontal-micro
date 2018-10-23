205a082c11811e6045be01bea601c2dff2462ef8
stevest <stamatiad.st@gmail.com>
2018-10-23 11:07:23 +0300

    RUN AUTOCOMMIT

--- experiment/network/run.sh
+++ experiment/network/run.sh
@@ -135,12 +135,12 @@ loccl="1"
 cluster="0"
 #for pc2pc in $(seq 6 2 20); do
 #for pv2pc in $(seq 52 52); do
-#for excitbias in $(seq 1 8); do
+for excitbias in $(seq 10 1 20); do
 ##for gababfactor in $(seq 26 34); do
 for loccl in $(seq 1 1); do
 #for stimnoise in $(seq 0.1 0.1 0.5); do
 #for cluster in $(seq 0 19); do
-for inhibias  in $(seq 1 0.5 9); do
+#for inhibias  in $(seq 1 0.5 9); do
 ##for erf in "${erf_array[@]}"; do
 #	cluster="${run}"
 	if [ "$exp" == "1" ]; then
